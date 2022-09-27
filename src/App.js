ff4c02a423d75cafeae2037fe41bd6915bacfb89
ladel7 <107640356+ladel7@users.noreply.github.com>
2022-09-27 17:24:48 -0400

    Created search bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./App.css";
 
-function App() {
-  let city = "Paris";
-  let [temp, setTemp] = useState(null);
-  let apiKey = "eec790e544b831eb1307518e7e3d5c07";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showWeather);
+export default function App() {
+  const [city, setCity] = useState("New York");
+  const [weatherInfo, setWeatherInfo] = useState(null);
 
-  function showWeather(response) {
-    setTemp(response.data.main.temp);
+  function showWeather() {
+    let apiKey = "eec790e544b831eb1307518e7e3d5c07";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then((response) => {
+      setWeatherInfo({
+        temp: Math.round(response.data.main.temp),
+        description: response.data.weather[0].description,
+        humidity: response.data.main.humidity,
+        wind: Math.round(response.data.wind.speed),
+        icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+        cityName: response.data.name,
+      });
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    showWeather();
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
   }
 
   return (
     <div className="App">
-      <h2>test</h2>
-      <p>
-        The temperature in {city} is {temp}
-      </p>
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Type a city" onChange={updateCity} />
+        <input type="submit" value="search" />
+      </form>
+      {weatherInfo ? (
+        <ul>
+          <li>City: {weatherInfo.cityName}</li>
+          <li>Temperature: {weatherInfo.temp}°C</li>
+          <li>Description: {weatherInfo.description}</li>
+          <li>Humidity: {weatherInfo.humidity}%</li>
+          <li>Wind: {weatherInfo.wind} km/h</li>
+        </ul>
+      ) : (
+        showWeather()
+      )}
     </div>
   );
 }
-
-export default App;
